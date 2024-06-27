006e7ad082a7b891b5bf68eb77b9874e1d33f2d7
Zu-23 <h.zakaria52@gmail.com>
2024-06-27 18:39:50 +0100

    - Created Employee DTO class in Models

---
+++ EmployeesManagement/Models/EmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class EmployeeDto
+    {
+        public Guid Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Position { get; set; }
+        public string? Department { get; set; }
+    }
+}
