25f76efe090999173ee002ae6efc18052df90f41
Zu-23 <h.zakaria52@gmail.com>
2024-06-27 18:17:13 +0100

    - Created DbContextClass file in Data folder - Added Employee property in DbContextClass

---
+++ EmployeesManagement/Data/DbContextClass.cs
@@ -0,0 +1,14 @@
+﻿using EmployeesManagement.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagement.Data
+{
+    public class DbContextClass : DbContext
+    {
+        public DbContextClass(DbContextOptions<DbContextClass> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
