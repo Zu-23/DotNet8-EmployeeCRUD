554e0ee2feb0fb023aed48b29e910a4030abba76
Zu-23 <h.zakaria52@gmail.com>
2024-06-27 18:34:34 +0100

    - Created Employee controller and added GET request

---
+++ EmployeesManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using EmployeesManagement.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly DbContextClass _dbContext;
+        public EmployeeController(DbContextClass dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            var empList = _dbContext.Employees.ToList();
+            return Ok(empList);
+        }
+    }
+}
