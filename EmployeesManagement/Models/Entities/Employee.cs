12d4c4b088923f4c17e26c776fdfd66c69aed6ce
Zu-23 <h.zakaria52@gmail.com>
2024-06-27 18:10:50 +0100

    -Created Models/Entities folders -Created Employee class file in Models/Entities -Added attributes to Employee class

---
+++ EmployeesManagement/Models/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeesManagement.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Position { get; set; }
+        public string? Department { get; set; }
+    }
+}
