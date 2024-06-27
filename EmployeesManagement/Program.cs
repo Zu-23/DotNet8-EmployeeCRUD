67d4a34f5653116047e99552178572e6e36a2417
Zu-23 <h.zakaria52@gmail.com>
2024-06-27 18:24:48 +0100

    - Injecting DbContext to services

--- EmployeesManagement/Program.cs
+++ EmployeesManagement/Program.cs
@@ -1,6 +1,12 @@
+using EmployeesManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DbContextClass>(options => options.UseSqlite(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
